b3dbd6b6e4db2c2ff142d6a66b652a839087c14f
Evgenii Kolgin <mista.colgate@gmail.com>
2021-10-31 14:20:56 +0800

    make it better

--- generateData.js
+++ generateData.js
@@ -4,17 +4,14 @@ module.exports =  function(){
     var faker = require('faker');
     var lodash = require('lodash');
     return{
-        users:lodash.times(100, function(n){
+        users:lodash.times(10, function(n){
             return{
                 id: n,
                 name: faker.name.findName(),
                 company: faker.company.companyName(),
-                address: [faker.address.country(), faker.address.city(), faker.address.latitude(), faker.address.longitude()],
                 email: faker.internet.email(),
                 avatar: faker.image.avatar(),
-                car: [faker.vehicle.manufacturer(), faker.vehicle.model(), faker.vehicle.color()]
             }
         })
-
     }
 }
